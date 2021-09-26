c549440d2ba8d1129423bae8bba412e24e5ac319
kyleParks-byui <par20029@byui.edu>
2021-09-26 01:09:52 -0600

    Char Conector submit is borked

--- ACC/Python/IMDBImporter.py
+++ ACC/Python/IMDBImporter.py
@@ -96,7 +96,7 @@ def main():
     db_controller.create_connection()
     db_controller.create_db_if_not_exists()
     imdbImp = IMDBImporter(db_controller)
-    imdbImp.get_actor_IMDB(402)
+    imdbImp.get_actor_IMDB(636046)
 
 
 if __name__ == '__main__':
