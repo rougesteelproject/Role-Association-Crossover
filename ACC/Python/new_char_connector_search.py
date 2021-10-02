eb18145468854cae77287c93e71bfdf87cfdde59
kyleParks-byui <par20029@byui.edu>
2021-10-02 01:54:38 -0600

    todo list

---
+++ ACC/Python/new_char_connector_search.py
@@ -0,0 +1,16 @@
+class CharacterConnectorSearch:
+    def __init__(self, db_control, query1, query2):
+        self.db_control = db_control
+        self.query1 = query1
+        self.query2 = query2
+
+        self.displayed_mrs = []
+
+    def get_mrs():
+        pass
+        #TODO
+
+    def get_roles():
+        pass
+        #TODO
+        #Then ad to the MRS, look to wiki_gen
