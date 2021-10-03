db26a8165f3c7ed7bcc4ecb3afb5ebe2130a3f1d
kyleParks-byui <par20029@byui.edu>
2021-10-03 01:48:14 -0600

    found a bug. Fix db_cont swap id

--- ACC/Python/database_controller.py
+++ ACC/Python/database_controller.py
@@ -318,6 +318,7 @@ class DatabaseController():
             
             swap_id = self.select_max_where("actor_swap_id", "roles","parent_meta", parent_meta)[0]
             #TODO i think this gives each one it's own swap id?
+                #Bug confirmed.
             if swap_id is not None:
                 swap_id += 1
             else:
